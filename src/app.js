366c5ab4dc196af8971d5faccf3a9e96dac865ed
FrankNew <franknew2009@gmail.com>
2019-02-28 21:16:41 +0200

    start

--- src/app.js
+++ src/app.js
@@ -1,22 +1,44 @@
 function run(e) {
     e.preventDefault();
+
     let input = document.getElementById('input').value.split(/\n/).map( (v, id) => {
         let parsedV = v.split(' ');
         parsedV.unshift(id - 1);
 
         return parsedV;
     } );
+}
 
-    input.shift();
+function onChange(e) {
+    var input = event.target;
 
-    let outputArtem = artem(input);
-    let outputDenis = denis(input);
-    let outputAlexnder = alexander(input);
+    var reader = new FileReader();
 
-    document.getElementById('output').value = JSON.stringify(input);
+    console.log("change");
+    reader.onload = function(){
+        console.log("Load");
+        var text = reader.result;
+
+        let formatedInput = text.split(/\n/).map( (v, id) => {
+            let parsedV = v.split(' ');
+            parsedV.unshift(id - 1);
+
+            return parsedV;
+        } );
+
+        formatedInput.shift();
+
+        let outputArtem = artem(formatedInput);
+        let outputDenis = denis(formatedInput);
+        let outputAlexnder = alexander(formatedInput);
+
+    };
+
+    reader.readAsText(input.files[0]);
 }
 
 window.run = run;
+window.onChange = onChange;
 
 function artem(input) {
     console.log(input);
